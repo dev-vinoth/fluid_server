b0bd5232f913e5dcdae7bb2305f1241e360a8680
vinoth.murukesan <vinoth.murukesan@i3software.in>
2017-01-18 16:59:56 +0530

    Endpoint name changed

--- index.js
+++ index.js
@@ -15,29 +15,29 @@ app.get('/', function (req, res) {
   res.end("This api is not available")
 })
 
-app.get('/api/form1', function (req, res) {
-   fs.readFile( __dirname + "/JSONs/" + "default-values-in-template.json", 'utf8', function (err, data) {
+app.get('/api/jobs', function (req, res) {
+   fs.readFile( __dirname + "/JSONs/" + "jobs.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
    });
 })
 
-app.get('/api/form2', function (req, res) {
-   fs.readFile( __dirname + "/JSONs/" + "forms.json", 'utf8', function (err, data) {
+app.get('/api/customers', function (req, res) {
+   fs.readFile( __dirname + "/JSONs/" + "customers.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
    });
 })
 
-app.get('/api/form3', function (req, res) {
-   fs.readFile( __dirname + "/JSONs/" + "section-field-position.json", 'utf8', function (err, data) {
+app.get('/api/mapView', function (req, res) {
+   fs.readFile( __dirname + "/JSONs/" + "mapView.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
    });
 })
 
-app.get('/api/form4', function (req, res) {
-   fs.readFile( __dirname + "/JSONs/" + "multiple-show-hide-section-targets.json", 'utf8', function (err, data) {
+app.get('/api/inventory', function (req, res) {
+   fs.readFile( __dirname + "/JSONs/" + "inventory.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
    });
@@ -54,4 +54,3 @@ var server = app.listen(port, function () {
   console.log("Example app listening at http://%s:%s", host, port)
 
 })
-
